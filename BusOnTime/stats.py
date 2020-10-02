506192bf8fba1c51fe2cd5b3896a67d74c666d97
Amit Gabbay <amit.gabbay1@gmail.com>
2020-10-02 06:19:29 +0300

    small output naming change at /stats/main

--- BusOnTime/stats.py
+++ BusOnTime/stats.py
@@ -42,4 +42,30 @@ class GeneralStats(Resource):
 
         # Parse results and return as json
         output = stats_schema.dump(performance_measures)
-        return {'Trips': output}
+        return {'Performance': output}
+
+
+# class GeneralStats(Resource):
+#
+#     def get(self):
+#
+#         params = get_arguments(request.args, "date", "by")
+#
+#         if params['date'] in [None, 'all']:
+#             date_filter = True  # Ignore filtering by sending 'True' as condition)
+#         else:
+#             date_filter = date_cond(params['date'])
+#
+#         if params['by'] in [None, 'oper']:
+#             measure_type = Trip_Model.agency_id
+#         else:
+#             measure_type = Trip_Model.cluster_id
+#
+#         performance_measures = db.session.query(measure_type) \
+#             .add_columns(func.avg(Trip_Model.departure_delay.in_(range(0, 6))).label("performance")) \
+#             .filter(date_filter) \
+#             .group_by(measure_type).order_by(desc("performance"))
+#
+#         # Parse results and return as json
+#         output = stats_schema.dump(performance_measures)
+#         return {'Trips': output}
