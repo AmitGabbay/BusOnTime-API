38135fc6554b274ec6888396aece33213615ac45
Amit Gabbay <amit.gabbay1@gmail.com>
2020-12-25 03:16:33 +0200

    Changes to support heroku deployment, with a small sample sqlite db

--- BusOnTime/__init__.py
+++ BusOnTime/__init__.py
@@ -12,7 +12,7 @@ api = Api()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data_small.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     CORS(app)
 
