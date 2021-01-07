203938d1c504397f9203e0822b881844552135b6
Amit Gabbay <amit.gabbay1@gmail.com>
2021-01-08 01:09:57 +0200

    Changes to support deploying on VM

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from BusOnTime import create_app
 app = create_app()
 
 # In "Flask server" configurations, port and debug are set in the run configurations
-if __name__ == '__main__':
-    app.run(debug=True, port=4999)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=4999)
